48226da145bdb0ea264a004bee2fde310d35e2e2
SushanthPoojary <sushanthpoojary888@gmail.com>
2022-09-01 17:35:57 +0530

    added navbar component and styled them

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,44 @@
+import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
     return (
-        <div className={styles.container}>Navbar</div>
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <h1 className={styles.title}>Just Pizza!</h1>
+            </div>
+            <div className={styles.item}>
+                <ul className={styles.lists}>
+                    <li className={styles.list}>Home</li>
+                    <li className={styles.list}>Menu</li>
+                    <li className={styles.list}>About Us</li>
+                    <li className={styles.list}>Contact</li>
+                </ul>
+            </div>
+            <div className={styles.item}>
+                <div className={styles.telIcon}>
+                    <i class="fa-solid fa-phone"></i>
+                </div>
+                <div className={styles.texts}>
+                    <div className={styles.text}>Order Now</div>
+                    <div className={styles.text}>+91 385 235 5183</div>
+                </div>
+                <div className={styles.icons}>
+                    <div className={styles.icon}>
+                        <i class="fa-regular fa-user"></i>
+                    </div>
+                    <div className={styles.icon}>
+                        <div className={styles.iconItem}>
+                            <div className={styles.cart}>
+                                <i class="fa-solid fa-cart-shopping"></i>
+                            </div>
+                            <div className={styles.cartQuantity}>2</div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
